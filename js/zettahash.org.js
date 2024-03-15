0321cc3355356afb7039d40502fb0abf5b3d091f
vswee <github@sinaswee.com>
2024-03-15 15:44:45 +0000

    js

---
+++ js/zettahash.org.js
@@ -0,0 +1,161 @@
+let logoSVG = `<svg class="zh-logo animated" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 318 47" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><clipPath id="a"><path clip-rule="evenodd" d="m67989.8 46909.1h46.224v46.189h-46.224z"/></clipPath><clipPath id="b"><path clip-rule="evenodd" d="m91.508 216.009-16.008 86.258h376.552l-299.045 273.746h602.156l18.331-83.318h-394.203l301.039-276.686z"/></clipPath><clipPath id="c"><path clip-rule="evenodd" d="m815.854 300.051 16.737-84.042h-115.799l-92.888 84.042z"/></clipPath><clipPath id="d"><path clip-rule="evenodd" d="m17.15 491.971-16.737 84.042h115.798l92.889-84.042z"/></clipPath><clipPath id="e"><path clip-rule="evenodd" d="m621.318.949h94.005l-169.267 789.61h-94.004l41.063-192.217h94.146l27.135-126.58h-94.119l21.053-97.675 116.441-104.664 17.009-79.346h-94.141z"/></clipPath><clipPath id="f"><path clip-rule="evenodd" d="m303.105.949h94.004l-169.266 789.61h-94.005l41.064-192.217h94.146l41.649-194.287-116.06 101.004 36.025-184.335 98.084-.869 27.82-129.778h-94.141z"/></clipPath><g transform="matrix(6.86002 0 0 1 -466411 -46909.13215)"><path d="m67989.8 46909.1h46.224v46.189h-46.224z" fill="none"/><g clip-path="url(#a)"><g id="z-icon"><g transform="matrix(.00852708 0 0 .0584959 67989.8 46909.1)"><path d="m91.508 216.009-16.008 86.258h376.552l-299.045 273.746h602.156l18.331-83.318h-394.203l301.039-276.686z" fill="none"/><g clip-path="url(#b)"><path id="zed" d="m48.777 253.466h648.389-118.62l-313.072 283.662h-131.636 660.735" fill="none" stroke="#fff" stroke-width="94.02"/></g><path d="m815.854 300.051 16.737-84.042h-115.799l-92.888 84.042z" fill="none"/><g clip-path="url(#c)"><path id="horizontal_1" d="m578.546 258.03h286.018" fill="none" stroke="#fff" stroke-opacity=".5" stroke-width="94.02"/></g><path d="m17.15 491.971-16.737 84.042h115.798l92.889-84.042z" fill="none"/><g clip-path="url(#d)"><path id="horizontal_2" d="m-31.56 533.992h286.018" fill="none" stroke="#fff" stroke-opacity=".5" stroke-width="94.02"/></g><path d="m621.318.949h94.005l-169.267 789.61h-94.004l41.063-192.217h94.146l27.135-126.58h-94.119l21.053-97.675 116.441-104.664 17.009-79.346h-94.141z" fill="none"/><g clip-path="url(#e)"><path id="vertical_2" d="m676.956-28.386-187.559 860.148" fill="none" stroke="#fff" stroke-opacity=".5" stroke-width="94.02"/></g><path d="m303.105.949h94.004l-169.266 789.61h-94.005l41.064-192.217h94.146l41.649-194.287-116.06 101.004 36.025-184.335 98.084-.869 27.82-129.778h-94.141z" fill="none"/><g clip-path="url(#f)"><path id="vertical_1" d="m354.313-28.386-183.13 860.148" fill="none" stroke="#fff" stroke-opacity=".5" stroke-width="94.02"/></g></g></g><g id="zhash" fill="#fff" fill-rule="nonzero"><path d="m0-149.669h-89.823c-8.576 0-15.879 3.052-22.057 9.229-6.177 6.141-9.193 13.445-9.193 22.02v22.928c0 8.685 3.016 15.988 9.193 22.057 6.178 6.177 13.481 9.193 22.057 9.193h89.823c5.523 0 10.065 4.542 10.065 10.065v22.928c0 5.559-4.542 10.101-10.065 10.101h-89.823c-5.56 0-10.102-4.542-10.102-10.101v-1.998h-21.148v1.998c0 8.684 3.016 16.024 9.193 22.056 6.178 6.177 13.481 9.193 22.057 9.193h89.823c8.684 0 16.097-3.016 22.274-9.193s9.193-13.481 9.193-22.056v-22.928c0-8.539-3.016-15.879-9.193-22.02-6.177-6.177-13.59-9.193-22.274-9.193h-89.823c-5.56 0-10.102-4.542-10.102-10.102v-22.928c0-5.523 4.542-10.065 10.102-10.065h89.823c5.523 0 10.065 4.542 10.065 10.065v2.035h21.402v-2.035c0-8.575-3.016-15.879-9.193-22.02-6.177-6.177-13.59-9.229-22.274-9.229" transform="matrix(.0262725 0 0 .18023 68030.5 46950.1)"/><path d="m0 107.373h-99.888c-5.414 0-10.065-4.542-10.065-10.102v-32.993h109.953zm-10.065-128.521h-121.073v21.184h121.073c5.56 0 10.065 4.542 10.065 10.065v33.029h-131.138v54.141c0 8.685 3.053 16.025 9.23 22.057 6.141 6.177 13.444 9.193 22.02 9.193h121.326v-118.42c0-8.576-3.016-15.879-9.193-22.02-6.177-6.177-13.626-9.229-22.31-9.229" transform="matrix(.0262725 0 0 .18023 68026.3 46927)"/><path d="m0 107.373h-99.889c-5.414 0-10.101-4.542-10.101-10.102v-32.993h109.99zm-10.101-128.521h-121.037v21.184h121.037c5.559 0 10.101 4.542 10.101 10.065v33.029h-131.138v54.141c0 8.685 3.016 16.025 9.193 22.057 6.177 6.177 13.481 9.193 22.056 9.193h121.291v-118.42c0-8.576-3.016-15.879-9.193-22.02-6.177-6.177-13.59-9.229-22.31-9.229" transform="matrix(.0262725 0 0 .18023 68017 46927)"/><path d="m0-100.688h-100.034v-48.981h-21.148v198.65h21.148v-118.42c0-5.523 4.651-10.065 10.211-10.065h89.823c5.523 0 10.065 4.542 10.065 10.065v118.42h21.148v-118.42c0-8.575-3.016-15.879-9.193-22.02-6.177-6.177-13.481-9.229-22.02-9.229" transform="matrix(.0262725 0 0 .18023 68021.5 46941.3)"/><path d="m0-21.111h-109.954v-33.03c0-5.523 4.506-10.065 10.066-10.065h89.823c5.523 0 10.065 4.542 10.065 10.065zm-10.065-64.279h-89.823c-8.576 0-15.879 3.052-22.057 9.229-6.177 6.141-9.193 13.445-9.193 22.02v87.171c0 8.684 3.016 16.024 9.193 22.056 6.178 6.177 13.481 9.193 22.057 9.193h121.326v-21.147h-121.326c-5.56 0-10.066-4.542-10.066-10.102v-32.993h131.392v-54.178c0-8.575-3.052-15.879-9.229-22.02-6.141-6.177-13.59-9.229-22.274-9.229" transform="matrix(.0262725 0 0 .18023 68006.7 46938.5)"/><path d="m0-143.964h162.096l-162.096 136.297v28.597h185.788v-20.893h-161.988l161.988-136.189v-28.742h-185.788z" transform="matrix(.0262725 0 0 .18023 67997.7 46946.4)"/><path d="m0-198.651h-21.039v167.401c0 8.685 3.016 16.025 9.048 22.056 5.923 6.178 13.372 9.194 22.056 9.194h54.795v-21.148h-54.795c-5.414 0-10.065-4.542-10.065-10.102v-97.236h64.86v-21.184h-64.86z" transform="matrix(.0262725 0 0 .18023 68011.3 46950.1)"/><path d="m0-198.651h-21.039v167.401c0 8.685 3.016 16.025 9.084 22.056 5.923 6.178 13.336 9.194 22.02 9.194h54.795v-21.148h-54.795c-5.414 0-10.065-4.542-10.065-10.102v-97.236h64.86v-21.184h-64.86z" transform="matrix(.0262725 0 0 .18023 68008.6 46950.1)"/><path d="m0-82.229c-6.177-6.177-13.481-9.229-22.056-9.229h-99.998v-48.982h-21.184v198.651h21.184v-118.42c0-5.523 4.651-10.065 10.211-10.065h89.787c5.559 0 10.101 4.542 10.101 10.065v118.42h21.148v-118.42c0-8.576-3.016-15.879-9.193-22.02" transform="matrix(.0262725 0 0 .18023 68035.8 46939.6)"/></g></g></g></svg>`
+
+for (const logo of document.querySelectorAll(".zh-svg-logo")) {
+  logo.innerHTML = logoSVG
+  logo.addEventListener("mouseenter", () => {
+    logo.querySelector(".zh-logo").classList.remove("animated")
+    setTimeout(() => {
+      logo.querySelector(".zh-logo").classList.add("animated")
+    }, 100)
+  })
+}
+
+
+
+
+let path = window.location.pathname.indexOf("/") == 0 ? window.location.pathname.substring(1).split("/") : window.location.pathname
+
+//Allowing for locale subdirectory
+if (path.length == 3) {
+  const parent = path[1]
+  const identifier = path[2]
+  if (document.querySelector(`[parent="${parent}"]`)) {
+    if (document.querySelector(`[parent="${parent}"]`).querySelector(`[identifier="${identifier}"]`)) {
+      let parentEle = document.querySelector(`[parent="${parent}"]`)
+      let idEle = parentEle.querySelector(`[identifier="${identifier}"]`)
+      parentEle.setAttribute('style', `color: rgb(255, 255, 255); z-index: 901;`)
+
+      parentEle.querySelector('address').setAttribute('class', `pad-menu-on-hover w-dropdown-toggle w--open`)
+      parentEle.querySelector('address').setAttribute('aria-expanded', `true`)
+      parentEle.querySelector('.line-rounded-icon.dropdown-arrow-sidebar').setAttribute('style', `transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;`)
+      parentEle.querySelector('nav').setAttribute('style', `opacity: 1; display: block; width: 243px;`)
+      parentEle.querySelector('nav').setAttribute('class', `dropdown-list-sidebar w-dropdown-list w--open`)
+    }
+  }
+  //progenitor
+  else if (document.querySelector(`[progenitor*="${parent}:"]`)) {
+    try {
+      let parentEle = document.querySelector(`[progenitor*="${parent}:"]`)
+      let jsonIdent = parentEle.getAttribute("progenitor").split(":")[1]
+      parentEle.setAttribute('style', `color: rgb(255, 255, 255); z-index: 901;`)
+      parentEle.querySelector('address').setAttribute('class', `pad-menu-on-hover w-dropdown-toggle w--open`)
+      parentEle.querySelector('address').setAttribute('aria-expanded', `true`)
+      parentEle.querySelector('.line-rounded-icon.dropdown-arrow-sidebar').setAttribute('style', `transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;`)
+      parentEle.querySelector('nav').setAttribute('style', `opacity: 1; display: block; width: 243px;`)
+      parentEle.querySelector('nav').setAttribute('class', `dropdown-list-sidebar w-dropdown-list w--open`)
+
+      let idEle = parentEle.querySelector(`[identifier="${jsonIdent}"]`)
+      idEle.style.color = `var(--colours--primary-1)`
+    } catch (err) { }
+  }
+}
+
+
+
+for (const c2c of document.querySelectorAll(".click-to-copy")) {
+  c2c.addEventListener("click", (event) => {
+    let text = event.target.hasAttribute('data-copy') ? event.target.getAttribute('data-copy') : event.target.innerHTML
+    let temp = document.createElement('input')
+    let originalText = event.target.innerHTML
+    event.target.innerHTML = `${originalText} ✅`
+    temp.classList.add("temp-input")
+    document.body.append(temp)
+    temp.value = text
+    temp.select()
+    document.execCommand("copy")
+    document.querySelector('.temp-input').remove()
+    setTimeout(() => { event.target.innerHTML = `${originalText}` }, 2500)
+  })
+}
+for (const itm of document.querySelectorAll(".import-to-metamask")) {
+  itm.addEventListener("click", async () => {
+    try {
+
+      const wasAdded = await ethereum.request({
+        method: 'wallet_watchAsset',
+        params: {
+          type: 'ERC20',
+          options: {
+            address: '0x2c0e15190acb858bf74447928cbd8fb9709dcb19',
+            symbol: 'ZH',
+            decimals: 18,
+            image: 'https://uploads-ssl.webflow.com/65d4f59afabe990d05bd8b5f/65da72d13d4f02927b3d58d4_logo-download-large-circle-1024x1024.png',
+          },
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  })
+}
+
+
+
+try {
+  let localeObj = document.querySelector(".locale-link-item.w--current").innerHTML.split(" ")
+  let localeFlag = localeObj[0]
+  let localeName = localeObj[1].substring(0, 2).toUpperCase()
+
+  //document.documentElement.style.setProperty('--locale-flag', localeFlag); language-string-name
+  for (const lsc of document.querySelectorAll(".language-short-code")) { lsc.innerHTML = localeFlag }
+  for (const lsc of document.querySelectorAll(".language-string-name")) { lsc.innerHTML = localeName }
+} catch (err) { console.log('no current language detected') }
+
+
+
+if (document.querySelectorAll(".provider-flex").length > 0) {
+  let flex = document.querySelector(".provider-flex")
+  let parentFlex = document.querySelector(".provider-flex").parentNode
+  let newFlex = document.createElement("div")
+  newFlex.classList.add("provider-flex")
+  newFlex.classList.add("animation-reverse")
+  for (let i = flex.querySelectorAll(".provider-tile").length - 1; i >= 0; i--) {
+    let newTile = document.createElement("a")
+    newTile.setAttribute("target", "_blank")
+    newTile.setAttribute("href", flex.querySelectorAll(".provider-tile")[i].getAttribute('href'))
+    newTile.classList.add("provider-tile")
+    newTile.innerHTML = flex.querySelectorAll(".provider-tile")[i].innerHTML
+    newFlex.append(newTile)
+  }
+  parentFlex.append(newFlex)
+}
+
+if (document.querySelector(".tile-flex-animated-container")) {
+  let providerMainContainer = document.querySelector(".tile-flex-animated-container")
+  for (const parent of document.querySelectorAll(".provider-flex")) {
+    for (const tile of parent.querySelectorAll(".provider-tile")) {
+      let newTile = document.createElement("div")
+      newTile.setAttribute("class", `w-layout-hflex provider-tile`)
+      newTile.innerHTML = tile.innerHTML
+      parent.append(newTile)
+    }
+    parent.style.setProperty("--parent-max-width", `${providerMainContainer.offsetWidth}px`)
+  }
+}
+
+
+
+if (document.querySelectorAll(".em-portfolio-links").length > 0) {
+  for (const profile of document.querySelectorAll(".profile")) {
+    if (!profile.querySelector(".em-portfolio-links")) { continue }
+    let links = profile.querySelectorAll(".em-portfolio-links a.position")
+    if (links.length <= 0) { continue }
+    if (links.length == 1) {
+      let href = links[0].querySelector("[slug]").getAttribute('slug')
+      const loc = `/${window.location.pathname.split("/")[1]}/portfolio/${href}`
+      profile.style.cursor = 'pointer'
+      profile.addEventListener("click", () => { window.location = loc })
+    }
+  }
+}
+
+
+
+if (document.querySelectorAll("[alt*='banner-behind-text']").length >= 1) {
+  let div = document.querySelectorAll("[alt*='banner-behind-text']")[0].parentNode
+  let fig = document.querySelectorAll("[alt*='banner-behind-text']")[0].parentNode.parentNode
+  fig.style['margin-bottom'] = '-60px'
+  fig.style['position'] = 'relative'
+  fig.style['z-index'] = '-1'
+  div.classList.add('pseudo-gradient-overlay')
+}
